9205f72d822f82a8fc89d726370dd2d63ce49061
Jung Soo <jsh562@nyu.edu>
2019-12-07 01:17:16 -0500

    manage follows, manage tags

--- Backend/app.py
+++ Backend/app.py
@@ -298,10 +298,224 @@ def search_id(key):
     if (user):
         try:
             with conn.cursor() as cursor:
+                #check if the person exists
+                checkID = '''SELECT *
+                            FROM Person 
+                            WHERE username = %s
+                            '''
+                cursor.execute(checkID, (key))
+                searchUser = cursor.fetchall()
+                if(not searchUser):
+                    status = 400
+                    response['errMsg'] = 'User does not exist'
+                else:
+                    # get if the current user is following the searched person
+                    query = '''SELECT followstatus
+                                FROM Follow
+                                WHERE username_follower = %s AND username_followed = %s
+                                '''
+                    cursor.execute(query, (user, key))
+                    followstatus = cursor.fetchone()
+                    response["followstatus"] = followstatus
+
+        except Exception as error:
+            errorMsg = error.args
+            response["errMsg"] = errorMsg
+            status = 400
+
+    else:
+        response["errMsg"] = "You have to login"
+        status = 401
+        
+    result = jsonify(response)
+    result.status_code = status
+    return result
+
+@app.route('/search/<key>/addFollow', methods=['GET'])
+@jwt_required
+def addFollow(key):
+    response = {}
+    status = 200
+
+    user = get_jwt_identity()
+
+    if (user):
+        try:
+            with conn.cursor() as cursor:
+                # insert Follow entity
                 query = '''INSERT INTO Follow
                             VALUES (%s, %s, False)
                             '''
+                cursor.execute(query, (key, user))
+                conn.commit()
+
+        except Exception as error:
+            errorMsg = error.args
+            response["errMsg"] = errorMsg
+            status = 400
+
+    else:
+        response["errMsg"] = "You have to login"
+        status = 401
+        
+    result = jsonify(response)
+    result.status_code = status
+    return result
+
+@app.route('/follow_request', methods=['GET', 'POST'])
+@jwt_required
+def follow_request():
+    if request.method == 'GET':
+        response = {}
+        status = 200
+
+        user = get_jwt_identity()
+
+        if (user):
+            try:
+                with conn.cursor() as cursor:
+                    # insert Follow entity
+                    query = '''SELECT username_follower
+                                FROM Follow
+                                WHERE username_followed = %s AND followstatus = False
+                                '''
+                    cursor.execute(query, (user))
+                    follow_requests = cursor.fetchall()
+                    response["follow_requests"] = follow_requests
+
+            except Exception as error:
+                errorMsg = error.args
+                response["errMsg"] = errorMsg
+                status = 400
+
+        else:
+            response["errMsg"] = "You have to login"
+            status = 401
+            
+        result = jsonify(response)
+        result.status_code = status
+        return result
     
+    if request.method == 'POST':
+        response = {}
+        status = 200
+
+        user = get_jwt_identity()
+
+        post_data = request.get_json()
+
+        #grabs information from the forms
+        accept = post_data.get('accept')
+        follower = post_data.get('follower')
+
+        if (user):
+            try:
+                with conn.cursor() as cursor:
+
+                    if(accept):
+                        # accept follow request
+                        query = '''UPDATE Follow
+                                    SET followstatus = True
+                                    WHERE username_followed = %s AND username_follower = %s
+                                    '''
+                        cursor.execute(query, (user, follower))
+                        conn.commit
+                    
+                    else:
+                        # decline follow request
+                        query = '''DELETE FROM Follow
+                                    WHERE username_followed = %s AND username_follower = %s
+                                    '''
+                        cursor.execute(query, (user, follower))
+                        conn.commit
+
+
+            except Exception as error:
+                errorMsg = error.args
+                response["errMsg"] = errorMsg
+                status = 400
+
+        else:
+            response["errMsg"] = "You have to login"
+            status = 401
+            
+        result = jsonify(response)
+        result.status_code = status
+        return result
+
+@app.route('/feed/<photo_id>/addTag', methods=['POST'])
+@jwt_required
+def addTag(photo_id):
+    response = {}
+    status = 200
+
+    user = get_jwt_identity()
+
+    post_data = request.get_json()
+
+    #grabs information from the forms
+    tagged = post_data.get('tagged')
+
+    if (user):
+        try:
+            with conn.cursor() as cursor:
+                # if tagging oneself
+                if(user == tagged):
+
+                    query = '''INSERT INTO Tagged
+                                VALUES (%s, %s, True)
+                                '''
+                    cursor.execute(query, (user, photo_id))
+                    conn.commit()
+                else:
+                    # check if the tagged person has permission to view the photo
+                    poster = None
+                    permitted = 0
+                    query = '''SELECT photoPoster
+                                FROM Photo
+                                WHERE photoID = %s'''
+                    cursor.execute(query, (photo_id))
+                    data = cursor.fetchone()
+                    poster = data['photoPoster']
+
+                    query = '''SELECT count(*) AS cnt
+                                FROM Photo
+                                WHERE photoID = %s AND (EXISTS (SELECT *
+                                                                FROM SharedWithWhom
+                                                                WHERE groupOwner = %s AND
+                                                                photoID = %s AND
+                                                                member_username = %s)
+                                                        OR
+                                                        (allFollowers = True AND EXISTS(SELECT *
+                                                                                        FROM Follow
+                                                                                        WHERE username_followed = %s AND
+                                                                                        username_follower = %s AND
+                                                                                        followStatus = True)))'''
+                    cursor.execute(query, (photo_id, poster, photo_id, tagged, poster, tagged))
+                    data = cursor.fetchone()
+                    permitted = data['cnt']
+                    # if the user has permissio to access the photo, add tag
+                    if (permitted):
+                        insertTag = '''INSERT INTO Tagged
+                                        VALUES (%s, %s, False)
+                                        '''
+                        cursor.execute(insertTag, (tagged, photo_id))
+                        conn.commit()
+                    else:
+                        status = 400
+                        response['errMsg'] = 'This user cannot be tagged to the photo'
+
+        except Exception as error:
+            errorMsg = error.args
+            response["errMsg"] = errorMsg
+            status = 400
+
+    else:
+        response["errMsg"] = "You have to login"
+        status = 401
+        
+    result = jsonify(response)
+    result.status_code = status
     return result
 
         
